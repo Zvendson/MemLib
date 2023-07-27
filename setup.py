15a0956f29df22ffa467c22d53a9022f79f094ba
Zvend <25896065+Zvendson@users.noreply.github.com>
2023-07-27 08:34:35 +0200

    Update 1.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='MemLib',
-    version='1.0.0',
+    version='1.1.6',
     packages=['MemLib'],
     include_package_data=True,
     url='https://github.com/Zvendson/PyMemLib',
