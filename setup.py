12b3d65c9a35be599a38dd53eb3f8dcc9c197b87
Zvend <25896065+Zvendson@users.noreply.github.com>
2023-07-27 05:12:49 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+
+setup(
+    name='MemLib',
+    version='1.0.0',
+    packages=['MemLib'],
+    url='https://github.com/Zvendson/PyMemLib',
+    license='',
+    author='Zvendson',
+    author_email='',
+    description='Library to interact with the windows API and ctypes.'
+)
