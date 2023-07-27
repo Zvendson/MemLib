a8746a03edef30a075e8fca66899ca42b42ad68e
Zvend <25896065+Zvendson@users.noreply.github.com>
2023-07-27 05:12:21 +0200

    Update Exceptions.py

--- MemLib/Exceptions.py
+++ MemLib/Exceptions.py
@@ -6,6 +6,19 @@ from ctypes.wintypes import WCHAR
 from MemLib.Kernel32 import FormatMessageW, GetLastError
 
 
+class Not32BitException(Exception):
+    def __init__(self):
+        Exception.__init__(self, "Python is required to run in 32 bit mode!")
+
+
+class Not64BitException(Exception):
+    def __init__(self):
+        Exception.__init__(self, "Python is required to run in 64 bit mode!")
+
+
+class NoAdminPrivileges(Exception):
+    def __init__(self):
+        Exception.__init__(self, "Python has no admin privileges!")
 
 
 class Win32Exception(RuntimeError):
