509830aad86fda31dbc1d5ed6756ed2eee2c74fa
Zvend <25896065+Zvendson@users.noreply.github.com>
2023-07-27 07:57:13 +0200

    Fixed Decorator.Export

--- MemLib/Decorators.py
+++ MemLib/Decorators.py
@@ -13,40 +13,30 @@ from typing import Any, Callable
 from MemLib.Exceptions import NoAdminPrivileges, Not32BitException, Not64BitException
 
 
+__all__ = ["Export"]
 
 _STRING_TYPES = (type(b''), type(u''))
 
 
-def Export(*, module: str = None) -> Any:
+def Export(obj: Any) -> Any:
     """
     Simple decorator to add a function to the __all__ list.
 
-    :param module: The module it should get exported to.
+    :param obj: The object to export
     :returns: The wrapped object.
     """
 
-    def ReturnFunction(function: Any) -> Any:
-        nonlocal module
+    mod = sys.modules[obj.__module__]
 
-        if module is None or module not in sys.modules:
-            print('\n-> '.join(sys.modules))
-            module = function.__module__
+    if hasattr(mod, '__all__'):
+        mod.__all__.append(obj.__name__)
+    else:
+        mod.__all__ = [obj.__name__]
 
-        mod = sys.modules[module]
-
-        if hasattr(mod, '__all__'):
-            mod.__all__.append(function.__name__)
-        else:
-            mod.__all__ = [function.__name__]
-
-        print(f"Added '{function.__name__}' to module: '{mod}'")
-
-        return function
-
-    return ReturnFunction
+    return obj
 
 
-@Export(module="MemLib")
+@Export
 def Require32Bit(f: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         if calcsize("P") * 8 != 32:
@@ -56,7 +46,7 @@ def Require32Bit(f: Callable) -> Callable:
     return wrapper
 
 
-@Export(module="MemLib")
+@Export
 def Require64Bit(f: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         if calcsize("P") * 8 != 64:
@@ -66,7 +56,7 @@ def Require64Bit(f: Callable) -> Callable:
     return wrapper
 
 
-@Export(module="MemLib")
+@Export
 def RequireAdmin(f: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         if windll.shell32.IsUserAnAdmin() == 0:
@@ -76,7 +66,7 @@ def RequireAdmin(f: Callable) -> Callable:
     return wrapper
 
 
-@Export(module="MemLib")
+@Export
 def Deprecated(reason: Callable | str) -> Callable:
     """
     This is a decorator which can be used to mark functions
@@ -150,6 +140,3 @@ def Deprecated(reason: Callable | str) -> Callable:
 
     else:
         raise TypeError(repr(type(reason)))
-
-
-print(__name__)
