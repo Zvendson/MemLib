942bb9225933981136c1e1f8ed247691f42b8435
Zvend <25896065+Zvendson@users.noreply.github.com>
2023-07-27 05:11:19 +0200

    Added Require32Bit, Require64Bit, RequireAdmin

--- MemLib/Decorators.py
+++ MemLib/Decorators.py
@@ -2,3 +2,35 @@
 :platform: Windows
 """
 
+from ctypes import windll
+from struct import calcsize
+from typing import Any, Callable
+
+from MemLib.Exceptions import NoAdminPrivileges, Not32BitException, Not64BitException
+def Require32Bit(f: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        if calcsize("P") * 8 != 32:
+            raise Not32BitException()
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def Require64Bit(f: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        if calcsize("P") * 8 != 64:
+            raise Not64BitException()
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def RequireAdmin(f: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        if windll.shell32.IsUserAnAdmin() == 0:
+            raise NoAdminPrivileges()
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
