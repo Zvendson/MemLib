ec4abd00269f3c1c537d60954a8f22cdfe22157f
Zvend <25896065+Zvendson@users.noreply.github.com>
2023-10-26 14:43:39 +0200

    Added SetThreadDescription

--- MemLib/Kernel32.py
+++ MemLib/Kernel32.py
@@ -283,6 +283,24 @@ def GetThreadDescription(threadHandle: int) -> str:
     return ""
 
 
+def SetThreadDescription(threadHandle: int, threadDescription: str) -> bool:
+    """
+    Retrieves the description that was assigned to a thread by calling SetThreadDescription.
+
+    :param threadHandle: A handle to the thread.
+    :param threadDescription: A string that specifies the description of the thread.
+    :returns: If the function succeeds, the return value is the HRESULT that denotes a successful operation. If the
+              function fails, the return value is an HRESULT that denotes the error.
+
+
+    .. note:: **See also:**
+        `HRESULT Values <https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/705fb797-2175-4a90-b5a3
+        -3918024b10b8>`_
+    """
+
+    return _SetThreadDescription(threadHandle, threadDescription)
+
+
 def TerminateThread(threadHandle: int, exitCode: int) -> bool:
     """
     Retrieves the termination status of the specified thread.
@@ -916,6 +934,10 @@ _GetExitCodeThread.restype = BOOL
 _GetThreadDescription = windll.kernel32.GetThreadDescription
 _GetThreadDescription.argtypes = [HANDLE, LPWSTR]
 _GetThreadDescription.restype = DWORD
+
+_SetThreadDescription = windll.kernel32.SetThreadDescription
+_SetThreadDescription.argtypes = [HANDLE, LPCWSTR]
+_SetThreadDescription.restype = DWORD
 
 _GetPriorityClass = windll.kernel32.GetPriorityClass
 _GetPriorityClass.argtypes = [HANDLE]
