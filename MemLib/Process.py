d4285814ce57831ff0a3bb5b83974c905a41d7b3
Zvend <soerenganske@hotmail.de>
2023-11-12 20:13:03 +0100

    Reworked Process.Exists() using psutil

--- MemLib/Process.py
+++ MemLib/Process.py
@@ -8,16 +8,20 @@ from ctypes import Array, byref, pointer
 from ctypes.wintypes import BYTE, DWORD, WCHAR
 from typing import List, Literal, TYPE_CHECKING, Type, TypeVar
 
+from psutil import pid_exists
+
 from MemLib.Constants import (
     CREATE_SUSPENDED, MEM_COMMIT, MEM_RELEASE, NORMAL_PRIORITY_CLASS, PAGE_EXECUTE_READWRITE, PROCESS_ALL_ACCESS,
-    STILL_ACTIVE, TH32CS_SNAPMODULE, TH32CS_SNAPMODULE32, TH32CS_SNAPPROCESS, TH32CS_SNAPTHREAD,
+    TH32CS_SNAPMODULE, TH32CS_SNAPMODULE32, TH32CS_SNAPPROCESS, TH32CS_SNAPTHREAD,
 )
 from MemLib.Decorators import RequireAdmin
 from MemLib.Kernel32 import (
-    CloseHandle, CreateRemoteThread, CreateToolhelp32Snapshot, GetExitCodeProcess, GetPriorityClass,
     Module32First, Module32Next, NtQueryInformationProcess, NtResumeProcess, NtSuspendProcess, OpenProcess,
-    Process32First, Process32Next, QueryFullProcessImageNameW, ReadProcessMemory, SetPriorityClass, TerminateProcess,
-    Thread32First, Thread32Next, VirtualAllocEx, VirtualFreeEx, VirtualProtectEx, Win32Exception, WriteProcessMemory,
+    Process32First, Process32Next, QueryFullProcessImageNameW, ReadProcessMemory, RegisterWaitForSingleObject,
+    SetPriorityClass, TerminateProcess,
+    Thread32First, Thread32Next, UnregisterWait, VirtualAllocEx, VirtualFreeEx, VirtualProtectEx, WaitOrTimerCallback,
+    Win32Exception,
+    WriteProcessMemory,
 )
 from MemLib.Module import Module
 from MemLib.Structs import MODULEENTRY32, PEB, PROCESSENTRY32, ProcessBasicInformation, Struct, THREADENTRY32
@@ -79,7 +83,7 @@ class Process:
         :returns: True if the process exists, False otherwise.
         """
 
-        return GetExitCodeProcess(self._handle) == STILL_ACTIVE
+        return pid_exists(self._processId)
 
     @RequireAdmin
     def Open(self, processId: int) -> bool:
