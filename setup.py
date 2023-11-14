d60ffde964510ab3c8af267eda6db7fbe2e45537
Zvend <soerenganske@hotmail.de>
2023-11-14 13:25:59 +0100

    Update 1.3.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='MemLib',
-    version='1.3.7',
+    version='1.3.8',
     packages=['MemLib'],
     include_package_data=True,
     url='https://github.com/Zvendson/PyMemLib',
