23b426d79f404ff81aeaa83375e3c306381511ed
Zvend <soerenganske@hotmail.de>
2025-06-11 00:37:35 +0200

    Added typehints and docstring to decorators

--- MemLib/Decorators.py
+++ MemLib/Decorators.py
@@ -1,5 +1,10 @@
 """
 :platform: Windows
+
+Decorator utilities and platform enforcement helpers.
+
+This module provides decorators for timing, privilege checking, bitness checks,
+and function deprecation with warnings, tailored for Windows Python projects.
 """
 
 import inspect
@@ -8,36 +13,44 @@ import warnings
 from ctypes import windll
 from struct import calcsize
 from functools import wraps
-from time import sleep, time
-from typing import Any, Callable, Type
+from time import time
+from typing import Any, Callable
 
 from MemLib.Exceptions import NoAdminPrivileges, Not32BitException, Not64BitException
 
 
-_STRING_TYPES: tuple[Type[bytes], Type[str]] = (type(b''), type(u''))
+_STRING_TYPES: tuple[type, type] = (str, bytes)
+"""Tuple of recognized string types for decorator argument detection."""
 
 
 def func_timer(out: Callable[[str], Any]) -> Callable:
     """
-    Decorator to meassure the time of execution for the tagged function when called.
+    Decorator to measure the execution time of the wrapped function.
 
-    :param function: The function to wrap.
-    :param out: The function to call with the text output.
-    :returns: The callable wrapped function object.
+    Args:
+        out (Callable[[str], Any]): Callback function that will receive the timing output string.
+
+    Returns:
+        Callable: The wrapped function with timing instrumentation.
+
+    Example:
+        @func_timer(print)
+        def my_function():
+            ...
     """
 
     def decorator(function: Callable[[str], Any]) -> Callable:
+        @wraps(function)
         def wrap(*args, **kwargs):
-            argstr = ", ".join(["%r" % a for a in args]) if args else ""
-            kwargstr = ", ".join(["%s=%r" % (k, v) for k, v in kwargs.items()]) \
-                if kwargs else ""
-            comma = ", " if (argstr and kwargstr) else ""
-            fargs = "%s(%s%s%s)" % (function.__name__, argstr, comma, kwargstr)
+            arg_str: str   = ", ".join(["%r" % a for a in args]) if args else ""
+            kwarg_str: str = ", ".join(["%s=%r" % (k, v) for k, v in kwargs.items()]) if kwargs else ""
+            comma: str     = ", " if (arg_str and kwarg_str) else ""
+            fn_args: str   = "%s(%s%s%s)" % (function.__name__, arg_str, comma, kwarg_str)
 
-            ts = time()
-            result = function(*args, **kwargs)
+            ts: float = time()
+            result: Any = function(*args, **kwargs)
             te = time()
-            out('%s took: %2.4f sec' % (fargs, te - ts))
+            out(f"{fn_args} took: {te - ts:2.4f} sec")
 
             return result
 
@@ -46,52 +59,98 @@ def func_timer(out: Callable[[str], Any]) -> Callable:
     return decorator
 
 
-def require_32bit(f: Callable) -> Callable:
+def require_32bit(function: Callable[..., Any]) -> Callable[..., Any]:
+    """
+    Decorator to restrict execution to 32-bit Python interpreters.
+
+    Raises:
+        Not32BitException: If not running in a 32-bit Python process.
+
+    Returns:
+        Callable: The wrapped function.
+
+    Example:
+        @require_32bit
+        def some_func():
+            ...
+    """
+    """
+    Decorator: Raises Not32BitException if running in anything other than a 32-bit python process.
+    """
+    @wraps(function)
     def wrapper(*args, **kwargs):
         if calcsize("P") * 8 != 32:
             raise Not32BitException()
-        return f(*args, **kwargs)
+        return function(*args, **kwargs)
 
     return wrapper
 
 
-def require_64bit(f: Callable) -> Callable:
+def require_64bit(function: Callable) -> Callable:
+    """
+    Decorator to restrict execution to 64-bit Python interpreters.
+
+    Raises:
+        Not64BitException: If not running in a 64-bit Python process.
+
+    Returns:
+        Callable: The wrapped function.
+    """
+    @wraps(function)
     def wrapper(*args, **kwargs):
         if calcsize("P") * 8 != 64:
             raise Not64BitException()
-        return f(*args, **kwargs)
+        return function(*args, **kwargs)
 
     return wrapper
 
 
-def require_admin(f: Callable) -> Callable:
+def require_admin(function: Callable) -> Callable:
+    """
+    Decorator to require the process to be running with administrative privileges.
+
+    Raises:
+        NoAdminPrivileges: If the current user is not an administrator.
+
+    Returns:
+        Callable: The wrapped function.
+    """
+    @wraps(function)
     def wrapper(*args, **kwargs):
         if windll.shell32.IsUserAnAdmin() == 0:
             raise NoAdminPrivileges()
-        return f(*args, **kwargs)
+        return function(*args, **kwargs)
 
     return wrapper
 
 
 def deprecated(reason: Callable | str) -> Callable:
     """
-    This is a decorator which can be used to mark functions
-    as deprecated. It will result in a warning being emitted
-    when the function is used.
+    Decorator to mark functions or classes as deprecated.
+
+    Emits a DeprecationWarning when the decorated function or class is called.
+    Can be used both with and without a reason string.
 
     Credits to: https://stackoverflow.com/a/40301488
+
+    Usage:
+        @deprecated("Use another function instead")
+        def old_func(): ...
+
+        @deprecated
+        def really_old_func(): ...
+
+    Args:
+        reason (str | Callable): Reason for deprecation, or the function/class itself.
+
+    Returns:
+        Callable: The decorated function/class with deprecation warning.
+
+    Raises:
+        TypeError: If used with an unsupported argument type.
     """
 
     if isinstance(reason, _STRING_TYPES):
-
-        # The @deprecated is used with a 'reason'.
-        #
-        # .. code-block:: python
-        #
-        #    @deprecated("please, use another function")
-        #    def old_function(x, y):
-        #      pass
-
         def decorator(func1):
 
             if inspect.isclass(func1):
@@ -115,15 +174,6 @@ def deprecated(reason: Callable | str) -> Callable:
         return decorator
 
     elif inspect.isclass(reason) or inspect.isfunction(reason):
-
-        # The @deprecated is used without any 'reason'.
-        #
-        # .. code-block:: python
-        #
-        #    @deprecated
-        #    def old_function(x, y):
-        #      pass
-
         func2 = reason
 
         if inspect.isclass(func2):
